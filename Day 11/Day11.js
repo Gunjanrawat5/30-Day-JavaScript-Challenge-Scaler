f480d3a572f656fda8b0e003a7f51800dfa04fc8
Gunjan <gunjanrawat5@gmail.com>
2023-11-16 19:27:51 +0530

    Day 11

---
+++ Day 11/Day11.js
@@ -0,0 +1,10 @@
+//List properties of an Object
+
+const sampleObject = {
+    name: 'Gunjan',
+    age: 22,
+    occupation: 'Developer',
+    city: 'Mumbai'
+  };
+
+  console.log(Object.keys(sampleObject).toString())
