78066ae39e3c2d9660317669bd48d600b8eb6026
Gunjan <gunjanrawat5@gmail.com>
2023-11-25 02:14:00 +0530

    Day 19

---
+++ Day 19/Day19.js
@@ -0,0 +1,22 @@
+// **Challenge 19**
+// Problem Description:
+// Write a JavaScript function named allPositive that checks whether all numbers in a given array are positive using the every() method.
+// Inputs:
+// An array of numbers.
+// Outputs:
+// A boolean value. Return true if all numbers are positive, otherwise false.
+// Hints:
+// The every() method tests whether all elements in the array pass the test implemented by the provided function.
+// Solution Approach:
+// Use the every() method and provide a callback that checks if the number is positive.
+
+function allPositive(numbers) {
+    return numbers.every(function(number) {
+        return number > 0;
+    });
+}
+
+const positiveNumbers = [1, 2, 3, 4, 5];
+const mixedNumbers = [-1, 2, 3, -4, 5];
+console.log(allPositive(positiveNumbers));  
+console.log(allPositive(mixedNumbers));     
